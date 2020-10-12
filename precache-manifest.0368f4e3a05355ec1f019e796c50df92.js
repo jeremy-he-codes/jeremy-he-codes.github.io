ffd659a7ffa041bb3e24c97c6222e60f051c0a48
PCoder <mobile.passion2015@yandex.com>
2020-10-12 14:44:13 -0500

    Updates

---
+++ precache-manifest.0368f4e3a05355ec1f019e796c50df92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "470f3b3d6a8ef4162663",
+    "url": "/static/css/main.88ad0f09.chunk.css"
+  },
+  {
+    "revision": "470f3b3d6a8ef4162663",
+    "url": "/static/js/main.470f3b3d.chunk.js"
+  },
+  {
+    "revision": "a8f93b0b1ca711a29543",
+    "url": "/static/js/1.a8f93b0b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ccecc6717ac775a87b2e327ab1d57348",
+    "url": "/static/media/JeremyWang.ccecc671.png"
+  },
+  {
+    "revision": "245220b5e29523719243bb0459680ced",
+    "url": "/index.html"
+  }
+];
